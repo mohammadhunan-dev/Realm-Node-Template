3dc70c01db8234b278f7297e8a5c4365471064fd
Mohammad Hunan Chughtai <mohammad.hunan@10gen.com>
2021-07-26 16:27:15 -0400

    init

---
+++ app.js
@@ -0,0 +1,30 @@
+const Realm = require("realm");
+const BSON = require("bson");
+// Update this with your App ID
+const app = new Realm.App({ id: "<Your App ID>" });
+
+const CatSchema = {
+  name: "Task",
+  properties: {
+    _id: "objectId",
+    name: "string",
+  },
+  primaryKey: "_id",
+};
+
+
+async function run() {
+    const credentials = Realm.Credentials.anonymous();
+    await app.logIn(credentials);
+
+    const realm = await Realm.open({
+        schema: [CatSchema],
+        sync: {
+          user: app.currentUser,
+          partitionValue: "My Partition",
+        },
+      });
+    
+
+}
+run().catch((err) => console.log(`error occurred: ${JSON.stringify(err)}`));
